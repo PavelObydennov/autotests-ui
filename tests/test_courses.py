36d511b78335b9d190c51cb22c09e0298c4c0ec7
PavelObydennov <109856109+PavelObydennov@users.noreply.github.com>
2025-11-18 01:49:51 +0500

    Знакомство с PageComponent. Практика работы с Page Component

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,43 +1,18 @@
-from time import sleep
-
-from playwright.sync_api import expect, Page
 import pytest
-
 from pages.courses_list_page import CoursesListPage
-from pages.create_course_page import CreateCoursePage
-
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_create_course(chromium_page_with_state: Page, courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
-    create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
-    # Проверка наличия заголовка "Create course"
-    create_course_page.check_visible_create_course_title()
-    # Проверка, что кнопка создания курса недоступна для нажатия
-    create_course_page.check_disabled_create_course_button()
-    # Проверка, что отображается пустой блок для предпросмотра изображения
-    create_course_page.check_visible_image_preview_empty_view()
-    # Проверка, что блок загрузки изображения отображается в состоянии, когда картинка не выбрана
-    create_course_page.check_visible_image_upload_view()
-    # Проверка, что форма создания курса отображается и содержит значения по умолчанию
-    create_course_page.check_visible_create_course_form(title='',estimated_time='',description='',max_score='0',min_score='0')
-    # Проверка наличие заголовка "Exercises"
-    create_course_page.check_visible_exercises_title()
-    # Проверка наличия кнопки создания задания
-    create_course_page.check_visible_create_exercise_button()
-    # Проверка, что отображается блок с пустыми заданиями
-    create_course_page.check_visible_exercises_empty_view()
-    # Загрузка изображение для превью курса
-    create_course_page.upload_preview_image('./testdata/files/image.png')
-    # Проверка, что блок загрузки изображения отображает состояние, когда картинка успешно загружена
-    create_course_page.check_visible_image_upload_view()
-    # Заполнение формы создания курса
-    create_course_page.fill_create_course_form(title = "Playwright",estimated_time = "2 weeks",description = "Playwright",max_score = "100",min_score = "10")
-    # Нажатие на кнопку создания курса
-    create_course_page.click_create_course_button()
-    # Проверка наличия заголовка "Courses"
+def test_empty_courses_list(courses_list_page: CoursesListPage):
+    courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+    courses_list_page.navbar.check_visible("username")
+    courses_list_page.sidebar.check_visible()
     courses_list_page.check_visible_courses_title()
-    # Проверка наличие кнопки создания курса
+    courses_list_page.check_visible_empty_view()
     courses_list_page.check_visible_create_course_button()
-    # Проверка корректность отображаемых данных на карточке курса
-    courses_list_page.check_visible_course_card(index=0,title='Playwright',estimated_time = "2 weeks",max_score = "100",min_score = "10")
+    courses_list_page.click_create_course_button()
+
+
+
+
+
