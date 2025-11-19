6c35d082339c2b5b989c800b6ab3e8c7550e796a
PavelObydennov <109856109+PavelObydennov@users.noreply.github.com>
2025-11-20 02:09:52 +0500

    Практикуемся в реализации PageComponent. Практикуемся в написании компонентов с использованием паттерна PageComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -7,27 +7,21 @@ from pages.create_course_page import CreateCoursePage
 def test_create_course(courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
     create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
     # Проверка наличия заголовка "Create course"
-    create_course_page.check_visible_create_course_title()
+    create_course_page.create_course_toolbar_view.check_visible()
     # Проверка, что кнопка создания курса недоступна для нажатия
-    create_course_page.check_disabled_create_course_button()
-    # Проверка, что блок загрузки изображения отображается в состоянии, когда картинка не выбрана
-    # create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
+    create_course_page.create_course_toolbar_view.check_visible()
     # Проверка, что форма создания курса отображается и содержит значения по умолчанию
-    create_course_page.check_visible_create_course_form(title='',estimated_time='',description='',max_score='0',min_score='0')
-    # Проверка наличие заголовка "Exercises"
-    create_course_page.check_visible_exercises_title()
-    # Проверка наличия кнопки создания задания
-    create_course_page.check_visible_create_exercise_button()
-    # Проверка, что отображается блок с пустыми заданиями
-    # create_course_page.check_visible_exercises_empty_view()
+    create_course_page.create_course_form_component.check_visible_create_course_form(title='',estimated_time='',description='',max_score='0',min_score='0')
+    # Проверка наличие заголовка "Exercises" и кнопки
+    create_course_page.create_course_exercise_toolbar_view.check_visible()
     # Загрузка изображение для превью курса
     create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
     # Проверка, что блок загрузки изображения отображает состояние, когда картинка успешно загружена
     create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
     # Заполнение формы создания курса
-    create_course_page.fill_create_course_form(title = "Playwright",estimated_time = "2 weeks",description = "Playwright",max_score = "100",min_score = "10")
+    create_course_page.create_course_form_component.fill_create_course_form(title = "Playwright",estimated_time = "2 weeks",description = "Playwright",max_score = "100",min_score = "10")
     # Нажатие на кнопку создания курса
-    create_course_page.click_create_course_button()
+    create_course_page.create_course_toolbar_view.click_create_course_button()
     # Проверка наличия заголовка "Courses"
     courses_list_page.toolbar_view.check_visible()
     # Проверка корректность отображаемых данных на карточке курса
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -6,9 +6,9 @@ from pages.dashboard_page import DashboardPage
 @pytest.mark.regression
 def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
     registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-    registration_page.fill_registration_form(email='user2.name@gmail.com',username = 'user2name', password='password')
+    registration_page.registration_form.fill_registration_form(email='user2.name@gmail.com',username = 'user2name', password='password')
     registration_page.click_button()
-    dashboard_page.check_visible_dashboard()
+    dashboard_page.check_visible_dashboard_title()
 
 
 
