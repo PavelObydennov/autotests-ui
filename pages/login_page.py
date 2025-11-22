8bd5debd7fd399919cd248dd3db142ae0cb96d10
PavelObydennov <109856109+PavelObydennov@users.noreply.github.com>
2025-11-22 19:03:07 +0500

    Знакомство с PageFactory

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,8 @@
 from playwright.sync_api import Page, expect
-
 from components.authentication.login_form_component import LoginFormComponent
+from elements.button import Button
+from elements.link import Link
+from elements.text import Text
 from pages.base_page import BasePage
 
 class LoginPage(BasePage):
@@ -9,12 +11,27 @@ class LoginPage(BasePage):
 
         self.login_form = LoginFormComponent(page)
 
-        self.login_button = page.get_by_test_id('login-page-login-button')
-        self.registration_link = page.get_by_test_id('login-page-registration-link')
+
+        # self.login_button = page.get_by_test_id('login-page-login-button') До page factory
+        self.login_button = Button(page,'login-page-login-button', 'Login')
+
+        # self.registration_link = page.get_by_test_id('login-page-registration-link') До page factory
+        self.registration_link = Link(page, 'login-page-registration-link', 'Registration')
+
+        # self.wrong_email_or_password_alert = page.get_by_test_id('login-page-wrong-email-or-password-alert') До page factory
+        self.wrong_email_or_password_alert = Text(
+            page,
+            'login-page-wrong-email-or-password-alert',
+            'Wrong email or password'
+        )
 
 
-    def click_button(self):
+    def click_login_button(self):
         self.login_button.click()
 
     def click_registration_link(self):
         self.registration_link.click()
+
+    def check_visible_wrong_email_or_password_alert(self):
+        self.wrong_email_or_password_alert.check_visible()
+        self.wrong_email_or_password_alert.check_have_text('Wrong email or password')
--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,5 +1,4 @@
 import pytest
-from playwright.sync_api import Page
 from pages.login_page import LoginPage
 
 
@@ -16,5 +15,5 @@ from pages.login_page import LoginPage
 def test_wrong_email_or_password_authorization(login_page: LoginPage, email: str, password: str):
     login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
     login_page.login_form.fill_login_form(email=email, password=password)
-    login_page.click_button()
-    login_page.login_form.check_visible()
+    login_page.click_login_button()
+    # login_page.login_form.check_visible()
