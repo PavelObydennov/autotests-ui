432e2dd924fdd9ac540cba61f7eb3bddfc8ce1b2
PavelObydennov <109856109+PavelObydennov@users.noreply.github.com>
2025-11-27 02:22:38 +0500

    Добавление видео в Allure отчет

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,14 +1,16 @@
 import pytest
 from playwright.sync_api import Page, Playwright
-
+from _pytest.fixtures import SubRequest
 from pages.authentication.registration_page import RegistrationPage
+import allure
+from allure_commons.types import AttachmentType
+from tools.playwright.pages import initialize_playwright_page
 
 
 @pytest.fixture()
-def chromium_page(playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    yield browser.new_page()
-    browser.close()
+def chromium_page(request: SubRequest,playwright: Playwright) -> Page:
+    yield from initialize_playwright_page(playwright, test_name = request.node.name)
+
 
 @pytest.fixture(scope='session')
 def initialize_browser_state(playwright: Playwright)-> Page:
@@ -26,8 +28,17 @@ def initialize_browser_state(playwright: Playwright)-> Page:
     browser.close()
 
 @pytest.fixture
-def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state="browser-state.json")
-    yield context.new_page()
-    browser.close()
+def chromium_page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright) -> Page:
+    yield from initialize_playwright_page(playwright, test_name = request.node.name, storage_state="browser-state.json")
+    # browser = playwright.chromium.launch(headless=False)
+    # context = browser.new_context(storage_state="browser-state.json", record_video_dir='./videos')
+    #
+    # context.tracing.start(screenshots=True, snapshots=True, sources=True)
+    # page = context.new_page()
+    # yield page
+    #
+    # context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
+    # browser.close()
+    #
+    # allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace',extension='zip') # файл трейсинга
+    # allure.attach.file(page.video.path(), name='video', attachment_type=AttachmentType.WEBM) # Видео
